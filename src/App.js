0c8b5b649d31c6d183be0cd275b69e40f8010a20
Seamoon Pandey <hackedasocial@gmail.com>
2022-11-08 10:24:20 +0545

    form submission added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+  // function showInput() {
+  //   alert(username);
+  //   alert(password);
+  // }
+
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  function showInput() {
-    alert(username);
-    alert(password);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  function printValues(event) {
+    event.preventDefault()
+    var user = {
+      name: name,
+      username: username,
+      age: age,
+    }
+    console.log(user);
   }
 
   return (
     <div className="App">
-      <h1>React Forms</h1>
+      {/* <h1>React Forms</h1>
       <form action=''>
         <input type="text" placeholder="username" value={username} onChange={(e) => { setUsername(e.target.value) }} />
         <br />
@@ -19,7 +32,20 @@ function App() {
         <br />
         <button onClick={showInput}>show input</button>
 
+      </form> */}
+
+      <h1>React Forms  Submit</h1>
+
+      <form onSubmit={printValues}>
+        <input type="text" placeholder='name' value={name} onChange={(e) => { setName(e.target.value) }} />
+        <br />
+        <input type="text" placeholder='username' value={username} onChange={(e) => { setUsername(e.target.value) }} />
+        <br />
+        <input type="text" placeholder='age' value={age} onChange={(e) => { setAge(e.target.value) }} />
+        <br />
+        <input type="submit" value='submit' />
       </form>
+
     </div>
   );
 }
